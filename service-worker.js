f76f4112a70355c6473f34debb5bfe0b5cbe76c2
seboss44120 <seboss44@gmail.com>
2026-03-09 19:18:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+const CACHE="planning-cache";
 
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>{
+
+return cache.addAll([
+
+"/",
+"/index.html",
+"/planning.html",
+"/conges.html",
+"/css/style.css",
+"/js/app.js"
+
+])
+
+})
+
+)
+
+});

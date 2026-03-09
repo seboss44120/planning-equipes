edceb8ab71195f83b9abd751385203297816107a
seboss44120 <seboss44@gmail.com>
2026-03-09 19:18:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+const urlParams = new URLSearchParams(window.location.search);
+const team = urlParams.get("team");
+
+if(team){
+
+document.getElementById("teamTitle").innerText="Planning "+team;
+
+var calendarEl=document.getElementById("calendar");
+
+var calendar=new FullCalendar.Calendar(calendarEl,{
+
+initialView:"dayGridMonth",
+
+events:[]
+
+});
+
+calendar.render();
+
+}
+
+});
+
+function addShift(){
+
+let trigramme=prompt("Trigramme");
+
+let creneau=prompt("Créneau");
+
+let date=prompt("Date YYYY-MM-DD");
+
+alert("Créneau ajouté : "+trigramme+" "+creneau+" "+date);
+
+}
